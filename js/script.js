69a2eb053155838740b63c8779d387751bb2b5d3
carmelopalumbo <carmelopalumbo@outlook.it>
2022-10-21 12:31:53 +0200

    add-todo-list

--- js/script.js
+++ js/script.js
@@ -1,9 +1,31 @@
 // js
 
-/* L’utente clicca su un bottone che genererà una griglia di gioco quadrata.
-Ogni cella ha un numero progressivo, da 1 a 100.
-Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata. */
+/* L’utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero tra quelli compresi in un range:
+con difficoltà 1 => tra 1 e 100
+con difficoltà 2 => tra 1 e 81
+con difficoltà 3 => tra 1 e 49
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+I numeri nella lista delle bombe non possono essere duplicati.
+In seguito l’utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba. */
+
+/* TO-DO
+    GRIGLIA DI GIOCO
+    AGGIUNGERE CELLE ID
+    GENERARE BOMBE
+    AL CLICK DELLA CELLA
+        VERIFICARE SE HO CLICCATO UNA BOMBA
+        CONTARE I TENTATIVI
+        ACCENDERE LA CELLA
+        VERIFICARE SE HO FINITO I TENTATIVI
+    
+    FINE GIOCO
+        STAMPARE IL RISULTATO
+        BLOCCARE LA GRIGLIA
+        MOSTRARE TUTTE LE BOMBE
+
+AL CLICK DI PLAY RESETTARE LA GRIGLIA E GENERARE NUOVE BOMBE */
 
 
 //variabili
